f78c2f017436b5fc113a1addd4544b278cc5dc88
kim sungkuk <sungkuk5420@gmail.com>
2023-12-22 17:56:22 +0900

    소스코드 업로드 테스트

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 Kakao.init('6d395deceefb445d914a013269bef873'); // 사용하려는 앱의 JavaScript 키 입력
-
 function testFunc(){
       Kakao.Share.sendScrap({
         requestUrl: 'https://developers.kakao.com',
