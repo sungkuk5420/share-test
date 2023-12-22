d5f6f38ca21dfa859ff324fe7b898803a0013ec7
kim sungkuk <sungkuk5420@gmail.com>
2023-12-22 18:26:46 +0900

    퀘이샤 앱으로 대체

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,27 @@
+<template>
+  <q-page class="flex flex-center">
+    <q-btn label="공유하기" @click="shareKakao"></q-btn>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'IndexPage'
+})
+
+</script>
+<script setup>
+import { onMounted } from 'vue'
+
+onMounted(() => {
+  Kakao.init('6d395deceefb445d914a013269bef873'); 
+});
+
+ const shareKakao = ()=>{
+      Kakao.Share.sendScrap({
+        requestUrl: 'https://developers.kakao.com',
+        templateId: 102174,
+      });
+}
+</script>
